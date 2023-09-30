840549eed777e358f3cf724a48b4ecda4cc4a6f6
fmjp1012 <fmjp1012@gmail.com>
2023-09-30 21:08:44 +0900

    change config

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 
 int main() {
-  int n;
-  cin >> n;
-  
+  string s;
+  cin >> s;
+  vector<char> v, v_prev;
+
+  for (char c: s) {
+    v.push_back(c);
+  }
+
+
+  v_prev = v;
+  sort(v.begin(), v.end());
+  reverse(v.begin(), v.end());
+
+  if (v == v_prev) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
 }
