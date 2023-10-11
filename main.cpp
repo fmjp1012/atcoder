958d18e5f1d6f50f5d72ce67d8552a04242fc369
fmjp1012 <fmjp1012@gmail.com>
2023-10-12 06:17:35 +0900

    add chmax and chmin

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<class T> void chmax(T &a, T b) {
+  if (a < b) a = b;
+}
+
+template<class T> void chmin(T &a, T b) {
+  if (a > b) a = b;
+}
+
 int main() {
   
 }
