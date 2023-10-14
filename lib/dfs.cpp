47eb5f66aacd9fdd9a7f5ade329c2bb06125e77e
fmjp1012 <fmjp1012@gmail.com>
2023-10-15 04:30:19 +0900

    add graph search algorithm

---
+++ lib/dfs.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using Graph = vector<vector<int>>;
+
+struct Edge {
+  int to;
+  int w;
+  Edge(int to, int w) : to(to), w(w) {}
+};
+
+void dfs(Graph &G, int s) {
+  int n = (int)G.size();
+  vector<bool> seen(n, false);
+  stack<int> todo;
+
+  seen.at(s) = true;
+  todo.push(s);
+
+  while (!todo.empty()) {
+    int v = todo.top();
+    todo.pop();
+
+    for (auto x: G.at(v)) {
+      
+      if (seen.at(x) == true) continue;
+
+      seen.at(x) = true;
+      todo.push(x);
+    }
+
+  }
+}
+
+int main() {
+
+}
