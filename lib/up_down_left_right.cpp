8f2f640dcbd35c2dd3570bbe7d9929017d58b3c1
fmjp1012 <fmjp1012@gmail.com>
2023-10-21 12:02:21 +0900

    add up_down_left_right to lib

---
+++ lib/up_down_left_right.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int dx[] = {0, 0, -1, 1}; // Left, Right, Up, Down
+  int dy[] = {-1, 1, 0, 0}; // Corresponding y-axis moves for each x-axis move
+
+  for (int i = 0; i < 4; i++) {
+    
+  }
+}
